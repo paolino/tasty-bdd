435cd49f3c70781cffdf84b0deae6bc8779ab997
Paolo Veronelli <veronelli@global.de>
2017-11-17 09:49:35 +0100

    export fail fast supporti, some docs

--- src/Test/Tasty/Bdd.hs
+++ src/Test/Tasty/Bdd.hs
@@ -15,6 +15,9 @@ module Test.Tasty.Bdd (
     , givenAndAfter_
     , hoare
     , then_
+    , failFastIngredients
+    , failFastTester
+    , prettyDifferences
 ) where
 
 import Control.Arrow                   ((***))
@@ -26,7 +29,10 @@ import Data.TreeDiff
 import Data.Typeable                   (Proxy(..), Typeable)
 import Test.BDD.Language
 import Test.BDD.LanguageFree
-import Test.Tasty.Ingredients.FailFast (FailFast(..))
+import Test.Tasty                      (defaultMainWithIngredients)
+import Test.Tasty.Ingredients          (Ingredient)
+import Test.Tasty.Ingredients.Basic    (consoleTestReporter, listingTests)
+import Test.Tasty.Ingredients.FailFast (FailFast(..), failFast)
 import Test.Tasty.Options              (OptionDescription(..), lookupOption)
 import Test.Tasty.Providers            (IsTest(..), Progress(..), Result,
                                         TestTree, singleTest, testFailed,
@@ -35,12 +41,15 @@ import Text.PrettyPrint                (render)
 import Text.Printf                     (printf)
 import Text.Show.Pretty                (ppShow)
 
+-- | testable monads can map to IO a Tasty Result
 class (MonadCatch m, MonadIO m, Monad m, Typeable m) => TestableMonad m where
     runCase :: m Result -> IO Result
+
 
 instance TestableMonad IO where
     runCase = id
 
+-- | any testable monad can make a BDDTest a tasty test
 instance (Typeable t, TestableMonad m)
         => IsTest (BDDTest m t ()) where
     run os (BDDTest ts rup w) f = runCase $ do
@@ -63,25 +72,36 @@ instance (Typeable t, TestableMonad m)
             Nothing -> teardowns >> return (testPassed "")
     testOptions = Tagged [Option (Proxy :: Proxy FailFast)]
 
+-- | show a coloured difference of 2 values
 prettyDifferences :: (ToExpr a) => a -> a -> String
 prettyDifferences a1 a2 =
-  show $ ansiWlEditExpr $ exprDiff (toExpr a1) (toExpr a2)
+    show $ ansiWlEditExpr $ exprDiff (toExpr a1) (toExpr a2)
 
+-- internal exception to trigger visual inspection on output
 newtype EqualityDoesntHold = EqualityDoesntHold String deriving (Show, Typeable)
 
 instance Exception EqualityDoesntHold
 
 infixl 4 @?=
+-- | equality test which show pretty differences on fail
 (@?=) :: (ToExpr a, Eq a, Typeable a, MonadThrow m) => a -> a -> m ()
 a1 @?= a2 =
     if a1 == a2
     then return ()
     else throwM (EqualityDoesntHold (prettyDifferences a1 a2))
 
+-- | Bdd to TestTree tasty test
 testBdd ::
     (MonadIO m , TestableMonad m, Typeable t)
-    => String
-    -> (BDDTesting m t () -> BDDPreparing m t ())
-    -> TestTree
+    => String -- ^ test name
+    -> (BDDTesting m t () -> BDDPreparing m t ()) -- ^ bdd test definition
+    -> TestTree  -- ^ resulting tasty test
 testBdd s = singleTest s . interpret  . ($End)
 
+-- | default test runner fail-fast aware
+failFastTester :: TestTree -> IO ()
+failFastTester = defaultMainWithIngredients failFastIngredients
+
+-- | basic ingredients fail-fast aware
+failFastIngredients :: [Ingredient]
+failFastIngredients = [listingTests, failFast consoleTestReporter]

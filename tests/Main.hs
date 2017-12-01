90a9a31b93debb3193c131d42035cb60a2b7488f
Paolo Veronelli <veronelli@global.de>
2017-12-01 08:53:21 +0100

    remove typos

--- tests/Main.hs
+++ tests/Main.hs
@@ -24,8 +24,55 @@ import           Text.InterpolatedString.QM
 
 import           CaptureStdout
 
-ingredients :: [Ingredient]
-ingredients = [listingTests, failFast consoleTestReporter]
+main = defaultMain $ testGroup "All"
+    [
+    testCase "givens and givenandafter order is respected" $ do
+        let t app = defaultMain
+                $ testBehavior "Test sequence"
+                $ Given (app "First effect")
+                $ Given (app "Another effect")
+                $ GivenAndAfter (app "Aquiring resource" >> return "Resource 1")
+                                (app . ("Release "++))
+                $ GivenAndAfter (app "Aquiring resource" >> return "Resource 2")
+                                (app . ("Release "++))
+                $ When (app "Action returning" >> return ())
+                $ Then (\_ -> return ())
+                $ End
+        testTest t $ [
+                    "First effect",
+                    "Another effect",
+                    "Aquiring resource",
+                    "Aquiring resource",
+                    "Action returning",
+                    "Release Resource 2",
+                    "Release Resource 1"
+                    ]
+
+    , testCase "recursive before decorations are honored" $ do
+        let t app = defaultMain
+                $ beforeEach (app 0)
+                $ testGroup "g1"
+                    [ testCase "t1" $ app 1
+                    , testCase "t2" $ app 2
+                    , testGroup "g2"
+                        [ testCase "t3" $ app 3]
+                    , testCase "t4" $ app 4
+                    ]
+        testTest t [0,1,0,2,0,3,0,4]
+
+    , testCase "recursive after decorations are honored" $ do
+        let t app = defaultMain
+                $ afterEach (app 0)
+                $ testGroup "g1"
+                    [ testCase "t1" $ app 1
+                    , testCase "t2" $ app 2
+                    , testGroup "g2"
+                        [ testCase "t3" $ app 3]
+                    , testCase "t4" $ app 4
+                    ]
+        testTest t [1,0,2,0,3,0,4,0]
+    ]
+
 
 testTest :: (Show a, Eq a) => ((a -> IO ()) -> IO ()) -> [a] -> IO ()
 testTest t r' = do
@@ -37,65 +84,6 @@ testTest t r' = do
     r <- readMVar l
     r H.@?= reverse r'
 
-t1 :: TestTree
-t1 = testCase "givens and givenandafter order is respected" $ do
-    let t app = defaultMain
-            $ testBehavior "Test sequence"
-            $ Given (app "First effect")
-            $ Given (app "Another effect")
-            $ GivenAndAfter (app "Aquiring resource" >> return "Resource 1")
-                            (app . ("Release "++))
-            $ GivenAndAfter (app "Aquiring resource" >> return "Resource 2")
-                            (app . ("Release "++))
-            $ When (app "Action returning" >> return ())
-            $ Then (\_ -> return ())
-            $ End
-    testTest t $ [
-                "First effect",
-                "Another effect",
-                "Aquiring resource",
-                "Aquiring resource",
-                "Action returning",
-                "Release Resource 2",
-                "Release Resource 1"
-                ]
-
-t2 :: TestTree
-t2 = testCase "recursive before decorations are honored" $ do
-    let t app = defaultMain
-            $ beforeEach (app 0)
-            $ testGroup "g1"
-                [ testCase "t1" $ app 1
-                , testCase "t2" $ app 2
-                , testGroup "g2"
-                    [ testCase "t3" $ app 3]
-                , testCase "t4" $ app 4
-                ]
-    testTest t [0,1,0,2,0,3,0,4]
-
-
-t3 :: TestTree
-t3 = testCase "recursive after decorations are honored" $ do
-    let t app = defaultMain
-            $ afterEach (app 0)
-            $ testGroup "g1"
-                [ testCase "t1" $ app 1
-                , testCase "t2" $ app 2
-                , testGroup "g2"
-                    [ testCase "t3" $ app 3]
-                , testCase "t4" $ app 4
-                ]
-    testTest t [1,0,2,0,3,0,4,0]
-
-main = defaultMain $ testGroup "All" [t1, t2, t3]
-
-
-
-
-
-
-
-
 
 {-
 t2 :: TestTree
@@ -106,7 +94,6 @@ t2 = testBehavior "Exceedingly long running test"
     $ Then (\w -> tail w @?= "ffect" >> threadDelay 1000000)
     $ Then (\w -> init w @?= "Effec" >> threadDelay 1000000)
     $ End
-
 
 val :: Value
 val = object [
